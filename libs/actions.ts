22d305264b4a86eab1bc69a29a7fa156ebacc1ca
Amith-Jagannath <amithjagannath02@gmail.com>
2024-01-09 08:53:50 +0530

    vercel

--- libs/actions.ts
+++ libs/actions.ts
@@ -23,7 +23,7 @@ const apiUrl = isProduction
   ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ""
   : "http://127.0.0.1:4000/graphql";
 const apiKey = isProduction
-  ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || ""
+  ? "https://tudar-tulipu-main-amith-jagannath.grafbase.app/graphql" || ""
   : "letmein";
 const serverUrl = isProduction
   ? process.env.NEXT_PUBLIC_SERVER_URL
