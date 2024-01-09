27404fdb5674deb719efc586aae10400afca17d4
Amith-Jagannath <amithjagannath02@gmail.com>
2024-01-09 13:45:51 +0530

    vercel

--- libs/actions.ts
+++ libs/actions.ts
@@ -19,10 +19,10 @@ import { createUserMutation } from "../graphql";
 import { PostDeleteById } from "../graphql";
 import { PostForm, SessionInterface } from "@/common.types";
 const isProduction = process.env.NODE_ENV === "production";
-const apiUrl = isProduction
-  ? process.env.NEXT_PUBLIC_GRAFBASE_API_URL || ""
-  : "http://127.0.0.1:4000/graphql";
-const apiKey = "https://dribble-master-amith-jagannath.grafbase.app/graphql";
+const apiUrl = "https://dribble-master-amith-jagannath.grafbase.app/graphql";
+const apiKey = isProduction
+  ? process.env.NEXT_PUBLIC_GRAFBASE_API_KEY || ""
+  : "letmein";
 const serverUrl = isProduction
   ? process.env.NEXT_PUBLIC_SERVER_URL
   : "http://localhost:3000";
